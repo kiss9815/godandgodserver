d116195a79a31533caa569bf72dad313fb0d9845
kiss9815 <kiss9815@naver.com>
2016-06-03 19:18:20 +0900

    hi

--- godandgod/routes/post.js
+++ godandgod/routes/post.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 
 router.get('/',function(req,res,next){
-    console.log('hey');
-})
+    
+     res.json({
+            "result": {"message" : "회원가입이 정상적으로 처리되었습니다...",
+                        "post" : {"post_name": "안녕하세요" }
+                        
+                      }
+                
+        });
+    
+    
+});
 
 module.exports = router;
